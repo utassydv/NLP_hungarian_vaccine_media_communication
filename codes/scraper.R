39bb4b008a3971cc55ebec5ededf3e39314fbe62
David Utassy <utassydv@gmail.com>
2021-05-15 20:48:18 +0200

    add scraper codes

--- codes/scraper.R
+++ codes/scraper.R
@@ -1,6 +1,7 @@
 library(jsonlite)
 library(rvest)
 library(data.table)
+library(utils)
 
 source("codes/scraper_helpers.R")
 
@@ -9,12 +10,13 @@ source("codes/scraper_helpers.R")
 
 # Collect vaccination related articles
 articles_origo <- fromJSON('https://www.origo.hu/api/article-search?h=www.origo.hu&h=kepek.origo.hu&iai=true&q=vakcina&hits=8000')$result
-articles_origo <- head(articles_origo) #TODO run without this
+pb <- txtProgressBar(min = 0, max = nrow(articles_origo), style = 3)
 
 # Scrape text from the given urls
-articles_origo$content<- lapply(articles_origo$url,function(x){
-  t <- read_html(x)
+articles_origo$content<- lapply(seq_along(articles_origo$url),function(i){
+  t <- read_html(articles_origo$url[[i]])
   article_text <- t %>% html_nodes('p') %>% html_text()
+  setTxtProgressBar(pb, i)
   return(article_text)
 }
 )
@@ -24,17 +26,32 @@ saveRDS(articles_origo, file = 'data/raw/articles_origo.rds')
 
 
 # www.index.hu ------------------------------------------------------------
-# TODO:
-# notes
-# some json is there but cannot expand url-s with curl :(
 
+articles_index <- get_urls_from_index(84)
+articles_index <- filter(articles_index, date_time > 2020)
+
+pb <- txtProgressBar(min = 0, max = nrow(articles_index), style = 3)
+articles_index$content <- lapply(seq_along(articles_index$url),function(i){
+  #Sys.sleep(3) # sleep is set otherwise Index blocks me
+  
+  t<- read_html(articles_index$url[[i]])
+  article_text <- t %>% 
+    html_nodes('.anti_xsl p , p+ p , div+ p') %>% 
+    html_text()
+  print(articles_index$url[[i]])
+  setTxtProgressBar(pb, i)
+  return(article_text)
+}
+)
+
+saveRDS(articles_index, file= 'data/raw/articles_index')
 
 # www.telex.hu ------------------------------------------------------------
 
+# Collect vaccination related articles
 articles_telex <- get_telex_urls('vakcina', 53)
 
-#articles_telex <- head(articles_telex)
-
+# Scrape text from the given urls
 articles_telex$content<- lapply(articles_telex$url,function(x){
   t <- read_html(x)
   article_text <- t %>% html_nodes('.article-html-content') %>%html_nodes('p') %>% html_text()
@@ -43,3 +60,42 @@ articles_telex$content<- lapply(articles_telex$url,function(x){
 )
 
 saveRDS(articles_telex, file = 'data/raw/articles_telex.rds')
+
+
+# www.24.hu ---------------------------------------------------------------
+
+# Collect vaccination related articles
+articles_24hu <- get_24hu_urls('vakcina', 42)
+articles_24hu <- head(articles_24hu)
+pb <- txtProgressBar(min = 0, max = nrow(articles_24hu), style = 3)
+
+# Scrape text from the given urls
+articles_24hu$content <- lapply(seq_along(articles_24hu$url), function(i){
+  Sys.sleep(1) # sleep is set otherwise 24.hu blocks me
+  t <- read_html(articles_24hu$url[[i]])
+  article_text <- t %>% html_nodes('.post-body p , p+ ul li') %>% html_text()
+  setTxtProgressBar(pb, i)
+  return(article_text)
+}
+)
+
+saveRDS(articles_24hu, file= 'data/raw/articles_24hu.rds')
+
+# www.444.hu ---------------------------------------------------------------
+
+# Collect vaccination related articles
+articles_444hu <- get_444hu_urls('vakcina', 28)
+pb <- txtProgressBar(min = 0, max = nrow(articles_444hu), style = 3)
+
+# Scrape text from the given urls
+articles_444hu$content <- lapply(seq_along(articles_444hu$url),function(i){
+  Sys.sleep(1) # sleep is set otherwise 444 blocks me
+  t<- read_html(articles_444hu$url[[i]])
+  article_text <- t %>% html_node('#content-main') %>% html_nodes('p') %>% html_text()
+  article_text <- article_text[1:length(article_text)-1]
+  setTxtProgressBar(pb, i)
+  return(article_text)
+}
+)
+
+saveRDS(articles_444hu, file= 'data/raw/articles_444hu.rds')
--- codes/scraper_helpers.R
+++ codes/scraper_helpers.R
@@ -1,9 +1,51 @@
+
+# index.hu helpers --------------------------------------------------------
+require(httr)
+library(dplyr)
+library(utils)
+
+get_urls_from_index <- function(number_of_pages){
+  pb <- txtProgressBar(min = 0, max = number_of_pages, style = 3)
+  articles_df <- data.frame(list._mod_ts=character(), list.cim=character(), list.url=character())
+  for ( page in 0:number_of_pages){
+    data = list(
+      `datum` = '2021-05-15',
+      `rovat` = '24ora/',
+      `url_params[ig]` = '2021-05-15',
+      `url_params[pepe]` = '1',
+      `url_params[rovat]` = '',
+      `url_params[s]` = 'vakcina',
+      `url_params[tol]` = '1999-01-01',
+      `url_params[alllowRovatChoose]` = '0',
+      `url_params[profil]` = '',
+      `url_params[cimke]` = '',
+      `url_params[word]` = '1',
+      `url_params[p]` = page
+    )
+    t <- POST('https://index.hu/api/json/', body = data)
+    df <- as.data.frame(fromJSON(content(t,'text', encoding = "UTF-8")))
+    
+    df <- df %>% 
+      select(list._mod_ts, list.cim, list.url)
+    
+    df$list.url <- paste0('https:',df$list.url)
+    
+    articles_df <- bind_rows(articles_df,df)
+    setTxtProgressBar(pb, page)
+  }
+  
+  articles_df <- rename(articles_df, 
+                        date_time=list._mod_ts, title=list.cim,url=list.url)
+  
+  return(articles_df)
+}
+
+# telex.hu helpers --------------------------------------------------------
 get_urls_from_one_page_telex  <- function(my_url) {
   print(my_url)
   t <- read_html(my_url)
   boxes <- t %>% html_nodes('.article')
   x <- boxes[[1]]
-  
   boxes_df <- lapply(boxes, function(x){
     t_list <- list()
     t_list[['title']] <- x %>% html_nodes(".list-item") %>% html_nodes("a.list-item__title") %>% html_text()
@@ -23,3 +65,59 @@ get_telex_urls <- function(searchterm, page_to_download) {
   ret_df <- rbindlist(lapply(links_to_get, get_urls_from_one_page_telex))
   return(ret_df)
 }
+  
+
+# 24.hu helpers -----------------------------------------------------------
+get_urls_from_one_page_24hu  <- function(my_url) {
+  print(my_url)
+  t <- read_html(my_url)
+  boxes <- t %>% html_nodes('.-hasImg')
+  x <- boxes[[1]]
+  boxes_df <- lapply(boxes, function(x){
+    t_list <- list()
+    t_list[['title']] <- x %>% html_nodes(".m-articleWidget__link") %>% html_text()
+    t_list[['url']] <- x %>% html_nodes(".m-articleWidget__link") %>% html_attr('href')
+    t_list[['date_time']] <- x  %>%  html_nodes(".a-date") %>% html_text()
+    return(data.frame(t_list))  
+  })
+  
+  df <- rbindlist(boxes_df)
+  return(df)
+}
+
+get_24hu_urls <- function(searchterm, page_to_download) {
+  # create links
+  links_to_get <- paste0('https://24.hu/page/', seq(1, page_to_download) ,'/?s=', searchterm)
+  ret_df <- rbindlist(lapply(links_to_get, get_urls_from_one_page_24hu))
+  return(ret_df)
+}
+
+# 444.hu helpers -----------------------------------------------------------
+my_url <- 'https://444.hu/kereses?q=vakcina&page=1'
+get_urls_from_one_page_444hu  <- function(my_url) {
+  print(my_url)
+  t <- read_html(my_url)
+  boxes <- t %>% html_nodes('.card')
+  x <- boxes[[1]]
+  boxes_df <- lapply(boxes, function(x){
+    t_list <- list()
+    texts <- x %>% html_nodes("a") %>% html_text()
+    t_list[['title']] <- texts[1]
+    refs <- x %>% html_nodes("a") %>% html_attr('href')
+    t_list[['url']] <- refs[1]
+    t_list[['date_time']] <- x  %>%  html_nodes(".byline__info") %>% html_text()
+    return(data.frame(t_list))  
+  })
+  
+  df <- rbindlist(boxes_df)
+  return(df)
+}
+
+get_444hu_urls <- function(searchterm, page_to_download) {
+  # create links
+  links_to_get <- paste0('https://444.hu/kereses?q=',searchterm, '&page=', seq(1, page_to_download))
+  ret_df <- rbindlist(lapply(links_to_get, get_urls_from_one_page_444hu))
+  return(ret_df)
+}
+
+  
